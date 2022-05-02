1f219003399ca5e27c787b6f67f7e815772f463d
arifaji29 <arifajif@gmail.com>
2022-05-02 20:22:03 +0700

    oke lur

---
+++ alert.js
@@ -0,0 +1,33 @@
+
+
+document.querySelector(".third").addEventListener("click", function() {
+  Swal.fire({
+    title: "SELAMAT HARI RAYA IDUL FITRI 1443 H.",
+    text: "Mohon Maaf Lahir & Batin, Maafin gak nih?",
+    imageUrl: "https://live.staticflickr.com/65535/52044813190_f7c402ff7b_b.jpg",
+    imageWidth: 550,
+    imageHeight: 300,
+    imageAlt: "Eagle Image",
+    showCancelButton: true,
+    cancelButtonText: "Maafin",
+    confirmButtonText: " Tetep Maafin",
+    confirmButtonColor: "#00ff55",
+    cancelButtonColor: "#999999",
+    reverseButtons: true,
+    
+
+  }
+  
+  );
+  // if (confirm===true){
+  //   alert('Terima Kasih');
+  // } else {
+  //   alert('ok');
+  // }
+}
+
+);
+
+
+
+
